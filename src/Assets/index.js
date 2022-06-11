b0863d0ba14c49b075b83ef70d609d3eb9b07e0b
M. Dzikri Alfarisyi <dikrialfarisi9@gmail.com>
2022-06-11 09:56:18 +0700

    add Redux

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -4,10 +4,8 @@ import SuccessAnim from './animations/success.json';
 import LoginImage from './images/login.png';
 import RegisterImage from './images/register.png';
 import SuccessImage from './images/success.png';
-import AnalyticsImage from './images/analytics.png'
-import CrashlyticImage from './images/crashlytic.png'
-import LogoutImage from './images/logout.png'
-import MapsImage from './images/maps.png'
+import LogoutImage from './images/logout.png';
+
 
 import IconName from './icons/name.png';
 import IconEmail from './icons/email.png';
@@ -22,10 +20,7 @@ export {
   LoginImage,
   RegisterImage,
   SuccessImage,
-  AnalyticsImage,
-  CrashlyticImage,
   LogoutImage,
-  MapsImage,
   IconName,
   IconEmail,
   IconPassword,
